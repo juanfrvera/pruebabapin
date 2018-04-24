7720fcda5909b684f6635f5832ddac6f226ccfcd
@aldoorsini <orsini.aldo@gmail.com>
2018-04-24 10:34:33 -0300

    Conditional landing page

--- Application/Security/Default.aspx
+++ Application/Security/Default.aspx
@@ -5,6 +5,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
     <title>BAPIN</title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
     <script>
 
         w = 800
@@ -14,23 +19,19 @@
             h = window.screen.availHeight;
         }
         var oDate = new Date();
-        var wOpen = window.open("../General/MessageSendPageList.aspx?qt=" + oDate.getTime() , "BAPIN", "width=" + (w - 10) + ",height=" + (h - 122) + ",top=0,left=0,scrollbars=yes,location=0");
+        //../General/MessageSendPageList.aspx
+        var wOpen = window.open("<%=RedirectPageInitial%>?qt=" + oDate.getTime(), "BAPIN", "width=" + (w - 10) + ",height=" + (h - 122) + ",top=0,left=0,scrollbars=yes,location=0");
         wOpen.resizeTo(w, h);
 
-    if(window.opener == null)
-    {
-        window.open('', '_self', '');
-        window.close();
-    }
+        if(window.opener == null)
+        {
+            window.open('', '_self', '');
+            window.close();
+        }
 
 
 
-    </script>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
-    
+    </script>    
     </div>
     </form>
 </body>
--- Application/Security/Default.aspx.cs
+++ Application/Security/Default.aspx.cs
@@ -10,14 +10,37 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using Service;
+using nc = Contract;
 
 namespace UI.Web.Security
 {
     public partial class Default : System.Web.UI.Page
     {
+        string redirectPageInitial;
+        public string RedirectPageInitial
+        {
+            get
+            {
+                if (redirectPageInitial == null)
+                    redirectPageInitial = "../General/MessageSendPageList.aspx";
+                return redirectPageInitial;
+            }
+            set
+            {
+                redirectPageInitial = value;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!this.IsPostBack)
+            {
+                redirectPageInitial = "../General/MessageSendPageList.aspx";
+                var result = MessageSendService.Current.GetResult(new nc.MessageSendFilter() { IsRead = false, IdUserTo = UIContext.Current.ContextUser.User.ID }).ToList();
+                if (result.Count <= 0)
+                    redirectPageInitial = "../Proyecto/ProyectoPageList.aspx";
+            }
         }
     }
 }
