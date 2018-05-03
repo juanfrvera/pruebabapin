ec683c95e95c37616ec4b342b1453e56d9bd2f6c
matiasracca <matiasracca@gmail.com>
2018-05-03 08:25:25 -0300

    Update BAPIN_v0.6_20180501.sql

--- Scripts/BAPIN_v0.6_20180501.sql
+++ Scripts/BAPIN_v0.6_20180501.sql
@@ -1,20 +1,20 @@
 -- select * from SistemaEntidadEstado where  IdSistemaEntidad = 437 order by IdEstado
 
-update SistemaEntidadEstado set Nombre = '1 - Idea/Perfil'
+update SistemaEntidadEstado set Nombre = '1-Idea/Perfil'
 where  IdSistemaEntidad = 437 and IdEstado = 1
 
-update SistemaEntidadEstado set Nombre = '2 - Prefact/Factib'
+update SistemaEntidadEstado set Nombre = '2-Prefact/Factib'
 where  IdSistemaEntidad = 437 and IdEstado = 3
 
-update SistemaEntidadEstado set Nombre = '5 - Cancelado'
+update SistemaEntidadEstado set Nombre = '5-Cancelado'
 where  IdSistemaEntidad = 437 and IdEstado = 9
 
-update SistemaEntidadEstado set Nombre = '3 - Iniciado'
+update SistemaEntidadEstado set Nombre = '3-Iniciado'
 where  IdSistemaEntidad = 437 and IdEstado = 16
 
-update SistemaEntidadEstado set Nombre = '4 - Finalizado'
+update SistemaEntidadEstado set Nombre = '4-Finalizado'
 where  IdSistemaEntidad = 437 and IdEstado = 50
 
-update Proceso set Nombre = '2 - Ampliación' where IdProceso = 2
-update Proceso set Nombre = '1 - Reposición' where IdProceso = 7
-update Proceso set Nombre = '3 - Equipamiento Básico de Oficina' where IdProceso = 23
+update Proceso set Nombre = '2 - AmpliaciÃ³n' where IdProceso = 2
+update Proceso set Nombre = '1 - ReposiciÃ³n' where IdProceso = 7
+update Proceso set Nombre = '3 - Equipamiento BÃ¡sico de Oficina' where IdProceso = 23
